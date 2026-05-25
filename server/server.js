1c79465c04391f9adbed69c04146677c4beecc4d
kaveri514 <chirumellakaveri@gmail.com>
2026-05-25 10:30:04 +0530

    fixed cors for deployment

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const bookingRoutes = require('./routes/bookings');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://eventora-mern-ivbf.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
@@ -30,5 +33,6 @@ app.get("*", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
